72448498bf6d502c7aea5eff21476ca3d8760d3a
officeBon <officeBon@qq.com>
2018-05-30 18:33:29 +0800

    新分支 重复造轮子

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,3 @@ export default {
   name: 'App'
 }
 </script>
-
-<style>
-@import "../static/css/main.css";
-@import "../static/css/color-dark.css";     /*深色主题*/
-/*@import "../static/css/theme-green/color-green.css";   浅绿色主题*/
-</style>
--- src/main.js
+++ src/main.js
@@ -1,16 +1,28 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+
+import 'normalize.css/normalize.css'// A modern alternative to CSS resets
 
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'    // 默认主题
-Vue.use(ElementUI)
+import 'element-ui/lib/theme-chalk/index.css'
+import locale from 'element-ui/lib/locale/lang/en' // lang i18n
 
-import axios from 'axios'
-axios.defaults.headers.post['Content-Type']='application/json';  //设置默认post为json格式
-Vue.prototype.$axios = axios;  //为axios设置全局方法变量
+import '@/styles/index.scss' // global css
+
+import App from './App'
+import router from './router'
+import store from './store'
+
+// import axios from 'axios'
+// axios.defaults.headers.post['Content-Type']='application/json';  //设置默认post为json格式
+// Vue.prototype.$axios = axios;  //为axios设置全局方法变量
+import {getRequest} from './utils/api'
+import {postRequest} from './utils/api'
+
+import '@/icons' // icon
+
+Vue.use(ElementUI, { locale })
 
 Vue.config.productionTip = false
 
@@ -18,6 +30,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  store,
+  template: '<App/>',
+  components: { App }
 })
