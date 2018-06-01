d6573ed9c657ce03d9bc6cb5bbb85c6e5db0845f
pengwen <502285815@qq.com>
2018-06-02 07:51:32 +0800

    用户列表功能完善

--- src/views/user/UserList.vue
+++ src/views/user/UserList.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="app-container calendar-list-container">
     <div class="filter-container">
-      <el-input style="width: 200px;" class="filter-item" placeholder="查询条件" >
+      <el-input style="width: 200px;" class="filter-item" placeholder="查询条件">
       </el-input>
-      <el-button class="filter-item" type="primary"  icon="el-icon-search" >搜索</el-button>
+      <el-button class="filter-item" style="margin-left: 10px;" type="primary" icon="el-icon-search">搜索</el-button>
+      <el-button @click="handleCreate" class="filter-item" style="margin-left: 10px;" type="primary"
+                 icon="el-icon-edit">
+        新增
+      </el-button>
     </div>
 
     <el-table :data="pageInfo.list" v-loading="listLoading" element-loading-text="给我一点时间" border fit
@@ -19,9 +23,9 @@
           <span>{{scope.row.gmtCreate | parseTime('{y}-{m}-{d} {h}:{i}')}}</span>
         </template>
       </el-table-column>
-      <el-table-column width="150px" label="姓名">
+      <el-table-column width="150px" align="center" label="姓名">
         <template slot-scope="scope">
-          <span class="link-type" @click="handleUpdate(scope.row)">{{scope.row.name}}</span>
+          <span class="link-type">{{scope.row.name}}</span>
         </template>
       </el-table-column>
       <el-table-column width="110px" align="center" label="登录名">
@@ -39,26 +43,59 @@
           <span>{{scope.row.address}}</span>
         </template>
       </el-table-column>
+      <el-table-column align="center" label="操作" width="230" class-name="small-padding fixed-width">
+        <template slot-scope="scope">
+          <el-button type="primary" size="mini" @click="handleUpdate(scope.row.userId)">修改</el-button>
+          <el-button type="danger" size="mini" @click="handleDel(scope.row.userId)">删除</el-button>
+        </template>
+      </el-table-column>
     </el-table>
 
     <pagination
       :pageSizes="pageSizes"
-      :pageSize="pageInfo.pageSize"
+      :pageSize="listParams.pageSize"
       :pageCount="pageInfo.total"
-      @pageSizeChange="handleSizeChange"
-      @currentPageChange="handleCurrentChange"></pagination>
+      @handleSizeChange="handleSizeChange"
+      @handleCurrentChange="handleCurrentChange"></pagination>
 
-    <!--<div class="pagination-container">-->
-      <!--<el-pagination background @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="listQuery.page" :page-sizes="[10,20,30, 50]" :page-size="listQuery.limit" layout="total, sizes, prev, pager, next, jumper" :total="total">-->
-      <!--</el-pagination>-->
-    <!--</div>-->
+    <el-dialog :title="dialogTitle" :visible.sync="dialogFormVisible">
+      <el-form ref="userForm" :rules="rules" :model="userParams" label-position="left" label-width="70px"
+               style='width: 400px; margin-left:50px;'>
+        <el-form-item  prop="username">
+          <el-input v-model="userParams.userId"></el-input>
+        </el-form-item>
+
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="userParams.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password" v-if="dialogTitle=='新增用户'">
+          <el-input type="password" v-model="userParams.password"></el-input>
+        </el-form-item>
+        <el-form-item label="姓名" prop="name">
+          <el-input v-model="userParams.name"></el-input>
+        </el-form-item>
+        <el-form-item label="电话" prop="phone">
+          <el-input v-model="userParams.phone"></el-input>
+        </el-form-item>
+        <el-form-item label="地址" prop="address">
+          <el-input v-model="userParams.address"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取消</el-button>
+        <el-button v-if="dialogTitle=='新增用户'" type="primary" @click="createUser">新增</el-button>
+        <el-button v-else type="primary" @click="updateUser">修改</el-button>
+      </div>
+    </el-dialog>
+
   </div>
 </template>
 
 <script>
-import Pagination from '@/components/Pagination'
+  import Pagination from '@/components/Pagination'
+
   export default {
-    components:{
+    components: {
       Pagination
     },
     data() {
@@ -69,9 +106,19 @@ import Pagination from '@/components/Pagination'
           pageNum: 1
         },
         pageSizes: [2, 5, 10, 20],
-        pageInfo:{
-          list:[]
+        pageInfo: {
+          list: []
         },
+        dialogFormVisible: false,
+        dialogTitle: '',
+        userParams: {
+        },
+        rules:{
+          username:[{required:true,message:'请输入用户名',trigger:'blur'}],
+          password:[{required:true,message:'请输入密码',trigger:'blur'}],
+          name:[{required:true,message:'请输入姓名',trigger:'blur'}],
+          phone:[{pattern:/^1(3|4|5|7|8)\d{9}$/,message:'请输入正确手机号码',trigger:'blur'}],
+        }
       }
     },
     created() {
@@ -83,19 +130,72 @@ import Pagination from '@/components/Pagination'
         this.listLoading = true;
         this.postRequest('/user/list', this.listParams).then(res => {
           _this.listLoading = false;
-          if(res.data.code=='00'){
+          if (res.data.code == '00') {
             this.pageInfo = res.data.data;
           }
         })
       },
       handleSizeChange(size) {
-        this.pageParams.pageSize = size;
-        this.getList(this.pageParams);
+        this.listParams.pageSize = size;
+        this.getList();
       },
       handleCurrentChange(currentPage) {
-        this.pageParams.pageNum=currentPage;
-        this.getList(this.pageParams);
-      }
+        this.listParams.pageNum = currentPage;
+        this.getList();
+      },
+      handleCreate() {
+        if (this.$refs['userForm']!==undefined) {
+          this.$refs['userForm'].resetFields();
+        }
+        this.dialogFormVisible = true;
+        this.dialogTitle = '新增用户';
+      },
+      createUser() {
+        this.postRequest('/user/addUser', this.userParams).then(res => {
+          if (res.data.code == '00') {
+            this.$message.success('新增成功');
+            this.dialogFormVisible = false;
+            this.getList();
+          }
+        })
+      },
+      handleUpdate(userId) {
+        if (this.$refs['userForm']!==undefined) {
+          this.$refs['userForm'].resetFields();
+        }
+        this.dialogFormVisible = true;
+        this.dialogTitle = '修改用户';
+        this.getRequest('/user/query?key='+userId).then(res => {
+          if (res.data.code == '00') {
+            this.userParams = res.data.data;
+          }
+        })
+      },
+      updateUser(){
+        this.postRequest('/user/updateUser', this.userParams).then(res => {
+          if (res.data.code == '00') {
+            this.$message.success('修改成功');
+            this.dialogFormVisible = false;
+            this.getList();
+          }
+        })
+      },
+      handleDel(userId) {
+        this.$confirm('是否删除该用户', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          this.getRequest('/user/deleteUser?key='+userId).then(res => {
+            if (res.data.code == '00') {
+              this.$message.success('删除成功');
+              this.getList();
+            }
+          })
+        }).catch(() => {
+          this.$message.info('已取消删除');
+        });
+      },
     }
   }
 
